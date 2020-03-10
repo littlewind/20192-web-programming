44275e516e7e3a482dbf65cb40a61d219851183e
littlewind <tranphung1412@gmail.com>
2020-03-10 10:53:02 +0700

    lab 2 - basic form

---
+++ Week2/basicform.php
@@ -0,0 +1,17 @@
+<html>
+    <head><title> A Simple Form</title></head>
+    <body>
+        <form action="Form4Radio.php" method="POST">
+            <br>
+            Enter email address: <input type="text" size="16" maxlength="100" name="email">
+            <br><br>
+            May we contact you?
+            <input type="radio" name="contact" value="Yes">
+            <input type="radio" name="contact" value="No">
+            <br><br>
+            <input type="SUBMIT" value="Click to Submit">
+            <input type="RESET" value="Erase and Restart">
+        </form>
+    </body>
+</html>
+
