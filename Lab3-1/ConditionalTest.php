f279908994ef9549e81107c035e2eeac9daf9095
littlewind <tranphung1412@gmail.com>
2020-03-17 10:41:50 +0700

    Lab 3-1 conditional test

---
+++ Lab3-1/ConditionalTest.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Conditional Test</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+        $first = $_GET["firstName"];
+        $middle = $_GET["middleName"];
+        $last = $_GET["lastName"];
+        print("Hi $first! Your fullname is $last $middle $first. <br><br/>");
+        if  ($first == $last) {
+            print("$first and $last are equal");
+        }
+        if  ($first < $last) {
+            print("$first is less than $last");
+        }
+        if  ($first > $last) {
+            print("$first is greater than $last");
+        }
+        print("<br><br/>");
+        
+        $grade1 = $_GET["grade1"];
+        $grade2 = $_GET["grade2"];
+        $final = (2*$grade1 + 3*$grade2) / 5;
+        
+        if ($final > 89) {
+            print("Your final grade is $final. You got an A. Congratulations!");
+        } elseif ($final > 79) {
+            print("Your final grade is $final. You got an B.");
+        } elseif ($final > 69) {
+            print("Your final grade is $final. You got an C.");
+        } elseif ($final > 59) {
+            print("Your final grade is $final. You got an D.");
+        } elseif ($final > 0) {
+            print("Your final grade is $final. You got an F.");
+        } else {
+            print("Illegal grade less than 0. Final grade = $final");
+        }
+        ?>
+    </body>
+</html>
